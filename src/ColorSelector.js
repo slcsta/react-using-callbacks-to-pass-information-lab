993083307d09209c899134e4ae86806f929e281f
Sarah <sarahlchavez@gmail.com>
2021-11-05 05:36:40 -0700

    commit tree 1fe561a2e4cb3a2785de5acca41a14e539ec6e0f

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -4,15 +4,16 @@ export default class ColorSelector extends Component {
 
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      let callback = () => this.props.setSelectedColor(str)
+      return <div onClick={callback} key={idx} className="color-swatch" style={{backgroundColor: str}}/>
     })
   )
-
+// for click events we have to add an event and provide a callback on the div element itself:
   render() {
     return (
-      <div id="colorSelector">
-        {this.makeColorSwatches()}
-      </div>
+    <div id="colorSelector">
+      {this.makeColorSwatches()}
+    </div>
     )
   }
 }
--- src/Matrix.js
+++ src/Matrix.js
@@ -7,10 +7,20 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      selectedColor: '#FFF'  // in this case, the default is set to #FFF (white)
+    }
+  }
+
+  //with a default color value set in state, we now need to add a method that can change that state value:
+  setSelectedColor = (newColor) => {
+    this.setState({
+      selectedColor: newColor  // this method updates selectedColor with whatever is passed into it as an argument
+    })
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor} />)
   )
 
   genMatrix = () => (
@@ -20,7 +30,7 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector setSelectedColor={this.setSelectedColor} />
         <div id="matrix">
           {this.genMatrix()}
         </div>
